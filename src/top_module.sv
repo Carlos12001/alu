4d6d67641846857621c90b915ca5aaeea59fd5f2
Carlos12001 <carlos.andres12001@gmail.com>
2023-03-30 01:12:05 -0600

    all finish

--- src/top_module.sv
+++ src/top_module.sv
@@ -5,32 +5,29 @@ module top_module(
     output logic [6:0] display_tens,
     output logic [6:0] display_sign,
     output logic [3:0] result,
-    output logic [3:0] flags
-);
-    parameter WIDTH = 4;
+    output logic [3:0] flags,
+	 output	logic [1:0] leftover_leds
+);	
+
     // Asignar las entradas de la ALU según el mapeo de los switches
-    logic a;
-    logic b;
-    logic uc;
-    logic [WIDTH-1:0] num_units;
-    logic [WIDTH-1:0] num_tens;
+    logic [3:0] a;
+    logic [3:0] b;
+    logic [3:0] uc;
+    logic [3:0] num_units;
+    logic [3:0] num_tens;
 
    always @(*) begin
+			leftover_leds = 2'b00;
         if (change_switches) begin
             uc = switches[7:4];
-			flags = a;
-            a = a;
-            b = b;
-        end else begin
-            uc = uc;
-			flags = 0;
+        end else begin;
             a = switches[7:4];
             b = switches[3:0];
         end
    end
 	
-/*	
-	alu #(WIDTH) dut(
+
+	alu #(4) dut(
         .a(a),
         .b(b),
         .uc(uc),
@@ -40,11 +37,10 @@ module top_module(
         .c(flags[1]),
         .v(flags[0])
     );
-	 */
 
 
      bin_converter uut_converter(
-        .bin({1'b0, a}),
+        .bin({flags[1], result}),
         .bin_unit(num_units),
         .bin_ten(num_tens)
     );
@@ -62,7 +58,7 @@ module top_module(
     );
 	 
 	sign_to_display uut_sign(
-		.n(1'b0),  
+		.n(flags[3]),  
 		.seg(display_sign)
 	);
 
